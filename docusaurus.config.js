0ec8973529343b57656d6a0167c15a403bbd7a50
terrafrost <terrafrost@php.net>
2026-08-21 06:33:21 -0500

    prep to make docs live

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,15 +23,17 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://phpseclib.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'phpseclib', // Usually your GitHub org/user name.
+  projectName: 'phpseclib.github.io', // Usually your repo name.
+  deploymentBranch: 'master',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
